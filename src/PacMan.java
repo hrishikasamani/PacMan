42f37dd55132f49fc10ecc3ea0f0a4fab701f71a
hrishikasamani <samani.hr@northeastern.edu>
2025-12-12 19:28:22 -0500

    make tiles

--- src/PacMan.java
+++ src/PacMan.java
@@ -94,6 +94,15 @@ public class PacMan extends JPanel {
         walls = new HashSet<Block>();
         foods = new HashSet<Block>();
         ghosts = new HashSet<Block>();
-        
+
+        for (int r = 0; r < rowCount; r++) {
+            for (int c = 0; c < columnCount; c++) {
+                String row = tileMap[r];
+                char tileMapChar = row.charAt(c);
+
+                int x = c*tileSize;
+                int y = r*tileSize;
+            }
+        }
     }
 }
