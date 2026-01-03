5f7d0c251c07f82ea7531778e8c0e44af12ea87f
hrishikasamani <samani.hr@northeastern.edu>
2026-01-02 22:24:42 -0500

    gameover

--- src/PacMan.java
+++ src/PacMan.java
@@ -329,6 +329,14 @@ public class PacMan extends JPanel implements ActionListener, KeyListener {
 
     @Override
     public void keyReleased(KeyEvent e) {
+        if (gameOver) {
+            loadMap();
+            resetPositions();
+            lives = 3;
+            score = 0;
+            gameOver = false;
+            gameLoop.start();
+        }
         //System.out.println("KeyEvent: " + e.getKeyCode());
         if (e.getKeyCode() == KeyEvent.VK_UP) {
             pacman.updateDirection('U');
