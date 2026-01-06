87fd203d726ced303793f368f05f4164da2ba2d6
hrishikasamani <samani.hr@northeastern.edu>
2026-01-06 17:47:32 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int rowCount = 21;
+        int columnCount = 19;
+        int tileSize = 32;
+        int boardWidth = columnCount * tileSize;
+        int boardHeight = rowCount * tileSize;
+    }
+}
