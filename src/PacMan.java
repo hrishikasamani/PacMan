d54d1f3cca657e3c53979b6ffb9942882e6e1628
hrishikasamani <samani.hr@northeastern.edu>
2026-01-11 16:02:18 -0500

    load pacman and food

--- src/PacMan.java
+++ src/PacMan.java
@@ -111,22 +111,31 @@ public class PacMan extends JPanel {
                 else if (tileMapChar == 'b') {
                     //blue ghost
                     Block ghost = new Block(blueGhostImage, x, y, tileSize, tileSize);
-                    ghosts.add(ghosts);
+                    ghosts.add(ghost);
                 }
                 else if (tileMapChar == 'o') {
                     //orange ghost
                     Block ghost = new Block(orangeGhostImage, x, y, tileSize, tileSize);
-                    ghosts.add(ghosts);
+                    ghosts.add(ghost);
                 }
-                else if (tileMapChar == 'o') {
+                else if (tileMapChar == 'p') {
                     //pink ghost
                     Block ghost = new Block(pinkGhostImage, x, y, tileSize, tileSize);
-                    ghosts.add(ghosts);
+                    ghosts.add(ghost);
                 }
-                else if (tileMapChar == 'o') {
+                else if (tileMapChar == 'r') {
                     //red ghost
                     Block ghost = new Block(redGhostImage, x, y, tileSize, tileSize);
-                    ghosts.add(ghosts);
+                    ghosts.add(ghost);
+                }
+                else if (tileMapChar == 'P') {
+                    //pacman
+                    pacman = new Block(pacmanRightImage, x, y, tileSize, tileSize);
+                }
+                else if (tileMapChar == ' ') {
+                    //food
+                    Block food = new Block(null, x + 14, y + 14, 4, 4);
+                    foods.add(food);
                 }
             }
         }
